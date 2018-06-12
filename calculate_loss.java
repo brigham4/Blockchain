710c537511ccd4754db98fac5c1cb171c8f37915
brigham4 <eric.brigham15@ncf.edu>
2018-06-12 10:23:44 -0400

    making program more helpful

--- calculate_loss.java
+++ calculate_loss.java
@@ -17,15 +17,21 @@ public class calculate_loss{
     }   
     public float calculate(){
         float p0 = (float) ( (float) (alpha - 2*(Math.pow(alpha, 2))))/( (float) (alpha*(2*Math.pow(alpha, 3) - (4*(Math.pow(alpha, 2)) + 1))));
+        System.out.println("p-naught is: " + p0);
         float p1 = (float) ( (float) (alpha - 2*(Math.pow(alpha, 2))))/( (float) (2*Math.pow(alpha, 3) - (4*(Math.pow(alpha, 2)) + 1)));
+        System.out.println("p-1 is: " + p1);
         float p0p = (float) ( (float) (1 - alpha)*(alpha - 2*(Math.pow(alpha, 2))))/( (float) (1 - 4*(Math.pow(alpha, 2) + 2*(Math.pow(alpha, 3)))));
+        System.out.println("p-naught-prime is: " + p0p);
         float prob_loss = (beta * (1 - p0 - p1)) + ((beta/(1-alpha))*p0p)*(alpha + (1-gamma) * (1 - alpha - beta));
         float proportion_lost = (beta-prob_loss)/beta;
-        System.out.println(prob_loss);
-        System.out.println(proportion_lost);
+        System.out.println("The probability that the block is lost is: " + prob_loss);
+        System.out.println("The proportion that the honest miner loses is: " + proportion_lost);
         return prob_loss;
     }
     public static void main(String[] args){
+        System.out.println("alpha read in: " + args[0]);
+        System.out.println("beta read in: " + args[1]);
+        System.out.println("gamma read in: " + args[2]);
         float alpha = Float.parseFloat(args[0]); 
         float beta = Float.parseFloat(args[1]);
         float gamma = Float.parseFloat(args[2]);
